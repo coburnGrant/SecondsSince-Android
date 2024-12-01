f498edcf11cdf391a03134254b208b13635ad888
Grant Coburn <140763612+coburnGrant@users.noreply.github.com>
2024-12-01 15:51:49 -0500

    Updated kotlin version

--- app/src/main/java/com/example/secondssince/ui/HelpScreen.kt
+++ app/src/main/java/com/example/secondssince/ui/HelpScreen.kt
@@ -4,8 +4,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -54,7 +54,8 @@ fun HelpScreen(
                 style = MaterialTheme.typography.bodyMedium,
                 modifier = Modifier.padding(vertical = 8.dp)
             )
-            Divider()
+
+            HorizontalDivider()
 
             Text(
                 text = "Preferences",
